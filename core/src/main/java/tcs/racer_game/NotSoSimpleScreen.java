794b912962f65b1254ca37bcef942750cd722543
WKSPPC\wksp2 <krzysiek6c25@gmail.com>
2025-09-01 21:13:08 +0200

    Centered camera

--- core/src/main/java/tcs/racer_game/NotSoSimpleScreen.java
+++ core/src/main/java/tcs/racer_game/NotSoSimpleScreen.java
@@ -1,5 +1,7 @@
 package tcs.racer_game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Color;
@@ -10,6 +12,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.*;
 import tcs.racer_game.car.Car;
+import tcs.racer_game.car.Direction;
 import tcs.racer_game.car.carv2.CarV2;
 
 public class NotSoSimpleScreen implements Screen {
@@ -52,6 +55,9 @@ public class NotSoSimpleScreen implements Screen {
         logo.draw(batch);
         car.draw(delta);
         batch.end();
+
+        camera.position.set(car.carPosition.x, car.carPosition.y, 0);
+
     }
 
     @Override
@@ -59,13 +65,18 @@ public class NotSoSimpleScreen implements Screen {
         // Online tutorials failed to provide better solution to center ExtendViewport
         // Feel free to refactor it
         viewport.update(width, height);
+
+        /// It appiers, if we centered camera on car, this is unnecesery,
+        /*
         float xPos = -((float) (viewport.getScreenWidth() - viewport.getScreenHeight()) / 2
                 * viewport.getWorldWidth() / viewport.getScreenWidth());
         float yPos = xPos > 0 ? -xPos : 0;
         xPos = xPos > 0 ? 0 : xPos;
         camera.position.set(viewport.getWorldWidth() / 2 + xPos, viewport.getWorldHeight() / 2 + yPos, 0); // Ustawienie kamery na środek
         camera.update();
+        */
     }
+
 
     @Override
     public void pause() {
